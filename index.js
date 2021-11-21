e4c21294545d4078c5e4f0199d2b1226085d6f7b
Darya <hukdarya@gmail.com>
2021-11-21 23:13:58 +0300

    hw9

---
+++ index.js
@@ -0,0 +1,39 @@
+// №1
+const arr = [2,3,4,5];
+let result = 1;
+
+for (let i = 0; i < arr.length; i++) {
+    result*=arr[i];
+}
+console.log(result);
+
+// // №2
+// const arr = [1,2,5,9,4,13,4,10];
+
+// for (let i = 0; i<arr.length; i++) {
+//     if (arr[i] === 4) {
+//         console.log('Found four');
+//         break;
+//     }
+// }
+
+// №3
+// const arr = [12,15,20,25,59,79];
+// result = 0;
+
+// for (let i = 0; i<arr.length; i++) {
+//     result+=arr[i];
+// }
+// result/=arr.length
+// console.log(result);
+
+// №4
+
+// const arr = [3,5,12,4,17];
+// for (let i = 0; i<arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i]+ '(even)');
+//     } else {
+//         console.log(arr[i] + '(odd)');
+//     }
+// }
